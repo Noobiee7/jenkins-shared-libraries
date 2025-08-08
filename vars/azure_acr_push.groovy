88d5a035fb147c590cbfa5e507a407f311557952
Noobiee7 <142085941+Noobiee7@users.noreply.github.com>
2025-08-09 00:41:21 +0530

    Update azure_acr_push.groovy

--- vars/azure_acr_push.groovy
+++ vars/azure_acr_push.groovy
@@ -1,4 +1,4 @@
-def call(String imageName, String dockerHubUser, String tagName, String name, Stirng acrName){
+def call(String imageName, String dockerHubUser, String tagName, String name, String acrName){
 withCredentials([usernamePassword(credentialsId: 'azure-sp', usernameVariable: 'ddac2575-c1ef-4003-8782-f382f95ffec1', passwordVariable: 'ibZ8Q~IJ-F-aseWV-wswcdyv6jtMxdtxdqnoIbEQ')]) {
         sh 'az login --service-principal -u $APP_ID -p $PASSWORD --tenant 11c0a18b-b9dd-43aa-b740-c285e47c2d2b'
     }
