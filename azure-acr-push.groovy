4f031f3ae9bef23624a4b77fd9cc29ee509b21f5
Noobiee7 <142085941+Noobiee7@users.noreply.github.com>
2025-08-08 17:39:04 +0530

    Delete azure-acr-push.groovy

--- azure-acr-push.groovy
+++
@@ -1,9 +0,0 @@
-def call(string imageName, string userName, string tagName, string name, stirng acrname, string servicePrinciple,  )
-withCredentials([usernamePassword(credentialsId: 'azure-sp', usernameVariable: 'APP_ID', passwordVariable: 'PASSWORD')]) {
-    sh '''
-        az login --service-principal -u $APP_ID -p $PASSWORD --tenant <TENANT_ID>
-        az acr login --name myregistry
-        docker tag shelby777/tetris:latest myregistry.azurecr.io/tetris:latest
-        docker push myregistry.azurecr.io/tetris:latest
-    '''
-}
