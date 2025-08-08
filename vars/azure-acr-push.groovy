548f3c3c1fb0b56618747878910ddbcd94509429
Noobiee7 <142085941+Noobiee7@users.noreply.github.com>
2025-08-08 17:57:44 +0530

    Update azure-acr-push.groovy

--- vars/azure-acr-push.groovy
+++ vars/azure-acr-push.groovy
@@ -1,6 +1,6 @@
-def call(string imageName, string userName, string tagName, string name, stirng acrName ){
-withCredentials([usernamePassword(credentialsId: 'azure-sp', usernameVariable: 'APP_ID', passwordVariable: 'PASSWORD')]) {
-        sh 'az login --service-principal -u $APP_ID -p $PASSWORD --tenant <TENANT_ID>'
+def call(string imageName, string userName, string tagName, string name, stirng acrName, ){
+withCredentials([usernamePassword(credentialsId: 'azure-sp', usernameVariable: 'ddac2575-c1ef-4003-8782-f382f95ffec1', passwordVariable: 'ibZ8Q~IJ-F-aseWV-wswcdyv6jtMxdtxdqnoIbEQ')]) {
+        sh 'az login --service-principal -u $APP_ID -p $PASSWORD --tenant 11c0a18b-b9dd-43aa-b740-c285e47c2d2b'
     }
     sh '''    
         az acr login --name $name
