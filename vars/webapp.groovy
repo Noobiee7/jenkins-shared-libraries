c0c7da9dd1ed04295ecbc25fba897407da398c4c
Noobiee7 <142085941+Noobiee7@users.noreply.github.com>
2025-08-09 13:24:22 +0530

    Update webapp.groovy

--- vars/webapp.groovy
+++ vars/webapp.groovy
@@ -4,11 +4,10 @@ def call(String appName, String rgName, String acrName, String imageName, String
     set +x
     az login --service-principal --username $AZURE_CLIENT_ID --password $AZURE_CLIENT_SECRET --tenant 11c0a18b-b9dd-43aa-b740-c285e47c2d2b
     """
-  #configure Web app
+  //configure Web app
   sh """
   az webapp config container set --name $appName --resource-group $rgName --docker-custom-image-name $acrName/$imageName:$tagName --docker-registry-server-url https://$acrName
 
-  #Restart web app
   az webapp restart --name $appName --resource-group $rgName
   
   """
