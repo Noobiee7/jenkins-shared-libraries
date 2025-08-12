78e686336b8f591c1f18b2b9a49c29fd60b92011
Noobiee7 <142085941+Noobiee7@users.noreply.github.com>
2025-08-12 20:49:29 +0530

    Update azure_acr_push.groovy

--- vars/azure_acr_push.groovy
+++ vars/azure_acr_push.groovy
@@ -1,4 +1,4 @@
-def call(String imageName, String dockerHubUser, String tagName, String name, String acrName){
+def call(String imageName, String dockerHubUser, String tagName, String acrName, String acrurl){
 withCredentials([usernamePassword(credentialsId: "acrCred", usernameVariable: "AZURE_CLIENT_ID", passwordVariable: "AZURE_CLIENT_SECRET")]) {
         sh """
         set +x
@@ -7,8 +7,8 @@ withCredentials([usernamePassword(credentialsId: "acrCred", usernameVariable: "A
         """
     }
     sh """    
-        az acr login --name $name
+        az acr login --name $carName
         docker tag $dockerHubUser/$imageName:$tagName $acrName/$imageName:$tagName
-        docker push $acrName/$imageName:$tagName
+        docker push $acrurl/$imageName:$tagName
     """
 }
