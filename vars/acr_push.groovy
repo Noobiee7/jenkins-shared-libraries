11a677f76b0b242965be27297a7d4ca29f751291
Noobiee7 <142085941+Noobiee7@users.noreply.github.com>
2025-08-12 20:45:21 +0530

    Create acr_push.groovy

---
+++ vars/acr_push.groovy
@@ -0,0 +1,11 @@
+def call(String acrName, String imageName, String tagName){
+  def call(String imageName, String dockerHubUser, String tagName, String name, String acrName){
+withCredentials([usernamePassword(credentialsId: "acrCred", usernameVariable: "AZURE_CLIENT_ID", passwordVariable: "AZURE_CLIENT_SECRET")]) {
+        sh """
+        set +x
+        az login --service-principal --username $AZURE_CLIENT_ID --password $AZURE_CLIENT_SECRET --tenant 11c0a18b-b9dd-43aa-b740-c285e47c2d2b 
+        set -x
+        """
+    }
+    sh "docker push $acrName/$imageName:$tagName"
+}
