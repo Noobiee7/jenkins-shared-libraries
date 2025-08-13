75e8a64db5837a11015872bc69da645757e0ac56
Noobiee7 <142085941+Noobiee7@users.noreply.github.com>
2025-08-13 18:49:44 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,7 +1,7 @@
 def call(String imageName, String tag, String acrName){
-  sh "
+  sh """
       docker build -t ${imageName}:${tag} .
         
       docker tag ${imageName}:${tag} ${acrName}/${imageName}:${tag}
-  "
+  """
 }
